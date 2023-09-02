a665a7d6750a7e0994a8937b665d5887dd2680c5
alex <alexko0321@gmail.com>
2023-09-02 17:16:29 +0800

    init

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 //export const API_BASE_URL = 'http://localhost:5207/api'; //localhost
-export const API_BASE_URL = 'https://partyroombooking.azurewebsites.net/api'; //deploy
+export const API_BASE_URL = 'https://partyroombooking.azurewebsites.net/api'; //deployment
